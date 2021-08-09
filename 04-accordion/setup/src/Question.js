2a433a6df1c5291c62b407e9e82eb4863cf0ceb7
Suleman Rai <suleman.rai@gmail.com>
2021-08-08 21:28:31 -0400

    initial commit

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,41 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+const Question = ({ id, info, title }) => {
+  const [visible, setVisisble] = useState(false)
 
-export default Question;
+  const toggleState = () => {
+    setVisisble(!visible)
+  }
+
+  const AiOutlinePlusReplacement = (props) => {
+    const {
+      props: {
+        attr: { pId, ...rest },
+        children: [defs, ...paths],
+      },
+    } = AiOutlinePlus()
+
+    const SAFE_PATHS = paths.map(
+      ({ type, props: { pId: pathId, ...pathProps } }) =>
+        React.createElement(type, pathProps)
+    )
+    const SAFE_CHILDREN = React.Children.toArray([defs, ...SAFE_PATHS])
+    const SAFE_PROPS = { attr: rest, ...props }
+
+    return React.cloneElement(AiOutlinePlus(), SAFE_PROPS, SAFE_CHILDREN)
+  }
+
+  return (
+    <article className='question' key={id}>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={toggleState}>
+          {visible ? <AiOutlineMinus /> : <AiOutlinePlusReplacement />}
+        </button>
+      </header>
+      {visible && <p>{info}</p>}
+    </article>
+  )
+}
+
+export default Question
