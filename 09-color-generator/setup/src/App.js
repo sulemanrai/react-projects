b7645722f8d187f9cabb6fc50f2ac56c1684ff17
Suleman Rai <suleman.rai@gmail.com>
2021-08-11 23:03:58 -0400

    initial commit

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,41 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('#f15025')
+  const [colors, setColors] = useState([])
+  const submitForm = (e) => {
+    e.preventDefault()
+    const Color = new Values(color)
+    setColors(Color.all())
+  }
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form>
+          <input
+            type='text'
+            placeholder='#f15025'
+            className='null'
+            onChange={(e) => {
+              setColor(e.target.value)
+            }}
+          />
+          <button type='submit' className='btn' onClick={(e) => submitForm(e)}>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {colors.map((color, index) => {
+          const { rgb, weight } = color
+          return (
+            <SingleColor rgb={rgb} weight={weight} index={index} key={index} />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ rgb, weight, index }) => {
+  const [r, g, b] = rgb
+  const [alert, setAlert] = useState(false)
+  function copy() {
+    var copyText = document.getElementById(index).children[1].innerText
+    navigator.clipboard.writeText(copyText)
+    setAlert(true)
+    setTimeout(function () {
+      setAlert(false)
+    }, 2000)
+  }
+  return (
+    <>
+      <article
+        id={index}
+        className={`color ${
+          weight >= 0 && index >= 8 ? 'color-light' : 'false'
+        }`}
+        style={{ backgroundColor: `${rgbToHex(r, g, b)}` }}
+        onClick={(index) => copy(index)}
+      >
+        <p className='percent-value'>{weight}</p>
+        <p className='color-value'>{rgbToHex(r, g, b)}</p>
+        {alert && <p className='alert'>copied to clipboard</p>}
+      </article>
+    </>
+  )
 }
 
 export default SingleColor
